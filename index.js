91a70228cc539aa01b30ae16c28310817a494ac0
manan228 <manan228@yahoo.in>
2025-02-01 02:32:03 -0800

    comfiguring port

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import jobRoute from "./routes/fetchJobs.routes.js";
 import { initialSetup } from "./utils/setSendgridAPIKey.js";
 
 const app = express();
-initialSetup()
+initialSetup();
 
 app.use("/", jobRoute);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
